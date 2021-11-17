c24b557401e0f825cdeb91c923aa120b69ebd693
thiago_vds <60550019+thiagovds@users.noreply.github.com>
2021-11-17 17:41:52 +0100

    added_email_sender

---
+++ CODE/ESP8266_email_sender/ESP8266_email_sender.ino
@@ -0,0 +1,283 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid = "Thai";   // Enter the namme of your WiFi Network.
+
+const char* password = "NotYourWifi33";  // Enter the Password of your WiFi Network.
+
+char server[] = "mail.smtp2go.com";   // The SMTP Server
+
+ 
+
+WiFiClient espClient;
+
+void setup()
+
+{
+
+  Serial.begin(115200);
+
+  delay(10);
+
+  Serial.println("");
+
+  Serial.println("");
+
+  Serial.print("Connecting To: ");
+
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+ 
+
+  while (WiFi.status() != WL_CONNECTED)
+
+  {
+
+    delay(500);
+
+    Serial.print("*");
+
+  }
+
+  Serial.println("");
+
+  Serial.println("WiFi Connected.");
+
+  Serial.print("IP address: ");
+
+  Serial.println(WiFi.localIP());
+
+  byte ret = sendEmail();
+
+ }
+
+ 
+
+void loop()
+
+{
+
+ 
+
+}
+
+ 
+
+byte sendEmail()
+
+{
+
+  if (espClient.connect(server, 2525) == 1)
+
+  {
+
+    Serial.println(F("connected"));
+
+  }
+
+  else
+
+  {
+
+    Serial.println(F("connection failed"));
+
+    return 0;
+
+  }
+
+  if (!emailResp())
+
+    return 0;
+
+ 
+
+  Serial.println(F("Sending EHLO"));
+
+  espClient.println("EHLO www.example.com");
+
+  if (!emailResp())
+
+    return 0;
+
+ 
+
+  Serial.println(F("Sending auth login"));
+
+  espClient.println("AUTH LOGIN");
+
+  if (!emailResp())
+
+    return 0;
+
+   
+
+  Serial.println(F("Sending User"));
+
+ 
+
+ 
+
+  espClient.println("YXJkdWlubzMzQHByb3Rvbm1haWwuY29t"); // Your encoded Username
+
+  if (!emailResp())
+
+    return 0;
+
+ 
+
+  Serial.println(F("Sending Password"));
+
+ 
+
+  espClient.println("YiYhTUZxNDJeIW03SyZN");// Your encoded Password
+
+  if (!emailResp())
+
+    return 0;
+
+ 
+
+  Serial.println(F("Sending From"));
+
+ 
+
+  espClient.println(F("MAIL From: arduino33@protonmail.com")); // Enter Sender Mail Id
+
+  if (!emailResp())
+
+    return 0;
+
+ 
+
+  Serial.println(F("Sending To"));
+
+  espClient.println(F("RCPT To: thiagovds7@hotmail.com")); // Enter Receiver Mail Id
+
+  if (!emailResp())
+
+    return 0;
+
+ 
+
+  Serial.println(F("Sending DATA"));
+
+  espClient.println(F("DATA"));
+
+  if (!emailResp())
+
+    return 0;
+
+  Serial.println(F("Sending email"));
+
+ 
+
+  espClient.println(F("To:  thiagovds7@hotmail.com")); // Enter Receiver Mail Id
+
+  // change to your address
+
+  espClient.println(F("From: arduino33@protonmail.com")); // Enter Sender Mail Id
+
+  espClient.println(F("Subject: ESP8266 test e-mail\r\n"));
+
+  espClient.println(F("This is is a test e-mail sent from ESP8266.\n"));
+
+  espClient.println(F("Second line of the test e-mail."));
+
+  espClient.println(F("Third line of the test e-mail."));
+
+  //
+
+  espClient.println(F("."));
+
+  if (!emailResp())
+
+    return 0;
+
+  //
+
+  Serial.println(F("Sending QUIT"));
+
+  espClient.println(F("QUIT"));
+
+  if (!emailResp())
+
+    return 0;
+
+  //
+
+  espClient.stop();
+
+  Serial.println(F("disconnected"));
+
+  return 1;
+
+}
+
+ 
+
+byte emailResp()
+
+{
+
+  byte responseCode;
+
+  byte readByte;
+
+  int loopCount = 0;
+
+ 
+
+  while (!espClient.available())
+
+  {
+
+    delay(1);
+
+    loopCount++;
+
+  
+
+    if (loopCount > 20000)
+
+    {
+
+      espClient.stop();
+
+      Serial.println(F("\r\nTimeout"));
+
+      return 0;
+
+    }
+
+  }
+
+ 
+
+  responseCode = espClient.peek();
+
+  while (espClient.available())
+
+  {
+
+    readByte = espClient.read();
+
+    Serial.write(readByte);
+
+  }
+
+ 
+
+  if (responseCode >= '4')
+
+  {
+
+   
+
+    return 0;
+
+  }
+
+  return 1;
+
+}
--- CODE/main/main.ino
+++ CODE/main/main.ino
@@ -2,7 +2,7 @@
 
 
 int mode;
-int time[];
+int time[2] = {0 , 0};
 
 void setup()
 {
@@ -15,17 +15,23 @@ void loop()
 	/*DEFINE FINITE STATE MACHINE: MODES
 	0 - insert schedule
 	1 - add pills to compartment
-	2 (default) - dispense pills
+	2 (default) - dispense pills+
+	+
+	+
+
 	*/
 	Serial.println("Please insert desired mode! press:");
 	Serial.println("0 - for inserting a schedule"); Serial.println("1 - for adding pills to a module storage"); Serial.println("any key - for dispensing pills");
 
-	Serial.read(mode);  //get input from user for mode
+	mode = Serial.read();  //get input from user in serial for mode, to be upgraded to a web service with ESP
 
 	switch (mode) {
 	    case 0:
-	      Serial.println("You are in insert schedule mode! \n Please insert the time '00:00' for the ");
-	      insert_schedule(time, module_number, medicine);  //module number corresponds to a type of medicine
+	      Serial.println("You are in insert schedule mode! \n Please insert the hours '00' followed by an enter and then minutes '00' "); 
+	      Serial.println("insert hours, range from 0 until 23:"); time[0] =Serial.read();
+	      Serial.print("insert minutes, range from 0 until 59");  time[1] =Serial.read();
+
+	      insert_schedule(*time, module_number, medicine);  //module number corresponds to a type of medicine
 	      break;
 
 	    case 1:
@@ -47,5 +53,18 @@ int photointerrupter()
 
 int alarm()
 {
+	
 	/* INSERT CODE FOR ALARM SYSTEM HERE (sound buzz) */
+
 }
+
+void dispense_pills()
+{
+	/* INSERT CODE FOR DISPENSING PILLS ALGORITHM HERE (sound buzz) */
+}
+
+void pass_data_to_ESP()
+{
+	
+}
+
