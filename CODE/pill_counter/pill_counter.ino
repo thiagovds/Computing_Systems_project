38769c126340cddf04c5c0f618de82157fb173c8
thiago_vds <60550019+thiagovds@users.noreply.github.com>
2021-11-17 02:23:47 +0100

    Update pill_counter.ino

--- CODE/pill_counter/pill_counter.ino
+++ CODE/pill_counter/pill_counter.ino
@@ -5,14 +5,14 @@ int calib_val = 900;   //calibration value, decrease if false counts
 int light = 0;
 int counter = 0;
 int error_count = 0;
-int long mytime[3] = {0,0,0};
+unsigned long mytime[] = {0,0,0};
 int i = 0;
 void setup() 
 {
-  Serial.println(mytime[i]);
   pinMode (LDR, INPUT); //define status ON/OFF
-  Serial.begin(9600);
-  Serial.println("Serial started");
+  Serial.begin(9600);     Serial.println("Serial started");
+  mytime[0] = millis();   Serial.println(mytime[i]);
+  i++;
 }
 
 void loop() 
@@ -23,48 +23,44 @@ void loop()
   {
     counter++; mytime[i] = millis();          //record passing of pill and its timestamp
     
-    if(mytime[i] - mytime[0] < 1000   &&   i != 0)   // if 2 pill counting events are less than 100ms apart, that means that 2 or more pills were dispensed at once!
+    if(mytime[i] - mytime[i-1] < 1000   &&   i != 0)   // if 2 pill counting events are less than 100ms apart, that means that 2 or more pills were dispensed at once!
     {
         Serial.println("DISPENSING ERROR! 2 or more pills dispensed!"); 
 
         //ADD LINE HERE TO SEND EMAIL!!!!!!           -------------------------------------------------------------------------------
 
-        Serial.print("counter : ");
-        Serial.println(counter);    
-        Serial.print("time difference : ") ;Serial.println(mytime[i]-mytime[0]);
-        
-        Serial.print("mytime[i] : ") ;Serial.println(mytime[i]);
-        Serial.print("mytime[0] : ") ;Serial.println(mytime[0]);
-        
-        Serial.print("i = ");Serial.println(i);   
-
-        delay(5000);  //5 seconds for user read  
+        i=0; mytime[i] = millis();  delay(4000);  //4 seconds for user read and reset mytime  
         error_count++;
+        
+        Serial.print("counter : ");           Serial.println(counter);    
+        Serial.print("time difference : ") ;  Serial.println(mytime[i]-mytime[i-1]);
+        Serial.print("mytime[i] : ") ;        Serial.println(mytime[i]);
+        Serial.print("mytime[0] : ") ;        Serial.println(mytime[i-1]);  
+        Serial.print("i = ");                 Serial.println(i);   
     }
-    else if (i == 2) { Serial.println(i); i = 0;}
-
-    Serial.print("counter value is : ");
-    Serial.println(counter, DEC);
-
-    Serial.print("photovoltage : ");
-    Serial.println(light,DEC);
-
-        Serial.print("time difference : ") ;Serial.println(mytime[i]-mytime[0]);
-        
-        Serial.print("mytime[i] : ") ;Serial.println(mytime[i]);
-        Serial.print("mytime[0] : ") ;Serial.println(mytime[0]);
-        
-        Serial.print("i = ");Serial.println(i);   
+    else
+    {
+      Serial.print("counter : ");             Serial.println(counter);    
+      Serial.print("time difference : ") ;    Serial.println(mytime[i]-mytime[i-1]);
+      Serial.print("mytime[i] : ") ;          Serial.println(mytime[i]);
+      Serial.print("mytime[0] : ") ;          Serial.println(mytime[i-1]);
+      Serial.print("i = ");                   Serial.println(i);   
+    }
     
-  if (counter >= 100){counter = 0;}
-  i++;
+    
+    
+    
+  
+    
+    
+  
+    
+    i++;
+    if (counter >= 100){counter = 0;}
   }
   else
   {
-  //  mytime[] = millis();
-  //  Serial.print("NO COUNT\n light =");
-  //  Serial.println(light);    
-  //  Serial.println(mytime[i]-mytime[0]);
-  //  Serial.print("i = ");Serial.println(i);        
+    //  Serial.print("NO COUNT\n light =");
+    //  Serial.println(light);            
   }
 }
